6692ae152b9a9f97ce8e81897ed9840cd303770f
Iker Garcia Ramirez <ikergarciadev@gmail.com>
2023-12-30 11:37:06 +0100

    feat(challenge1): ♻️ move and rename files

--- src/challenge1/index.test.ts
+++ src/challenge1/index.test.ts
@@ -1,13 +1,13 @@
-import { findFirstRepeated } from '../exercises/exercise1';
+import { findFirstRepeated } from '.';
 import { describe, expect, test } from 'vitest';
 
 describe('findFirstRepeated', () => {
   test('returns null for an empty array', () => {
-    expect(findFirstRepeated([])).toBeNull();
+    expect(findFirstRepeated([])).toBe(-1);
   });
 
   test('returns null for an array with no repeats', () => {
-    expect(findFirstRepeated([1, 2, 3, 4, 5])).toBeNull();
+    expect(findFirstRepeated([1, 2, 3, 4, 5])).toBe(-1);
   });
 
   test('returns the first repeated number for an array with repeats', () => {
